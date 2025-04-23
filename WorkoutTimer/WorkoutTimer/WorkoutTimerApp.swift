92d419250efbfb5f8a32309a417f1226b6b2cc9b
 菅野 冴花 <kansae@MacBook-Air.local>
2025-04-24 00:27:10 +0900

    Initial commit with Xcode project setup

---
+++ WorkoutTimer/WorkoutTimer/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer
+//
+//  Created by  菅野 冴花 on 2025/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
